53baf46073968e2617e2fd46ff02b546a75915bd
Mac <mac.kittipat@gmail.com>
2020-05-03 17:14:08 +0700

    Setup Lamda call SQS and SQS trigger Lambda

--- handler.js
+++ handler.js
@@ -1,18 +1,38 @@
-'use strict';
+const aws = require('aws-sdk');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID;
+const SQS_URL = 'https://sqs.ap-southeast-1.amazonaws.com/' + AWS_ACCOUNT_ID + '/SaveQueue';
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const sqs = new aws.SQS({region : 'ap-southeast-1'});
+
+exports.saveQueue = async (event) => {
+    var payload = event.body;
+    console.log('Saving message to SQS. params=' + JSON.stringify(payload));
+    var params = {
+        MessageBody: JSON.stringify(payload),
+        QueueUrl: SQS_URL
+    };
+    const sqsResponse = await sqs.sendMessage(params).promise();
+    console.log('Saved message to SQS completed. response=' + JSON.stringify(sqsResponse));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(sqsResponse, null, 2),
+    };
+};
+
+exports.saveDynamo = async (event) => {
+    console.log('Saving message to DynamoDB', JSON.stringify(event));
+
+    // var message = JSON.parse(event.body);
+    // var docClient = new AWS.DynamoDB.DocumentClient();
+    //
+    // docClient.put(params, function(err, data) {
+    //     if (err) {
+    //         console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    //     } else {
+    //         console.log("Added item:", JSON.stringify(data, null, 2));
+    //     }
+    // });
+
 };
